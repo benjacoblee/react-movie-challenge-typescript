300eda955737eedf2f168256fd712a8d71af27d3
benjacoblee <benjacoblee@gmail.com>
2020-12-28 17:49:37 +0800

    feat: filter working

--- src/components/Movies.tsx
+++ src/components/Movies.tsx
@@ -1,19 +1,49 @@
-import React, { Dispatch, SetStateAction, useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
+import queryString from "query-string";
 import { movie as MovieType } from "../types/movie";
 import { fetchMovies } from "../utils";
 import Movie from "./Movie";
 
-interface MoviesProps {}
+interface MoviesProps {
+    location: {
+        search: string;
+    };
+}
 
-const Movies: React.FC<MoviesProps> = () => {
+const Movies: React.FC<MoviesProps> = ({ location }) => {
     const [movies, setMovies] = useState<MovieType[]>([]);
     const [errors, setErrors] = useState<string>("");
 
+    const query: {
+        genre?: string;
+        productionYear?: string;
+    } = queryString.parse(location.search);
+
     useEffect(() => {
-        fetchMovies()
-            .then((moviesData) => setMovies(moviesData))
-            .catch((err) => setErrors(err.toString()));
-    }, []);
+        setMovies([]);
+        if (Object.keys(query).length > 0) {
+            fetchMovies().then((moviesData) => {
+                query.genre
+                    ? setMovies(
+                          moviesData.filter(
+                              (movie) => movie.genre === query.genre
+                          )
+                      )
+                    : setMovies(
+                          moviesData.filter(
+                              (movie) =>
+                                  movie.productionYear ===
+                                  parseInt(query.productionYear!)
+                          )
+                      );
+            });
+        } else {
+            fetchMovies()
+                .then((moviesData) => setMovies(moviesData))
+                .catch((err) => setErrors(err.toString()));
+        }
+        // eslint-disable-next-line
+    }, [location]);
 
     let body;
 

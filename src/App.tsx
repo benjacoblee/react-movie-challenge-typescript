c68bc3739706057b918e7cd001c22fdc2042f03f
benjacoblee <benjacoblee@gmail.com>
2020-12-28 14:01:33 +0800

    refactor: use alternate syntax for routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Movies from "./components/Movies";
+import MovieDetail from "./components/MovieDetail";
 
 const App: React.FC<{}> = () => {
     return (
         <Router>
             <Switch>
-                <Route path="/">
-                    <Movies />
-                </Route>
+                <Route exact path="/" component={Movies} />
+                <Route
+                    exact
+                    path="/movies/:movieName"
+                    component={MovieDetail}
+                />
             </Switch>
         </Router>
     );
